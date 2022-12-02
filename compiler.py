20a073b59996ccb7df0f432c858b6468afd4032a
Amir-NM <59971778+Amir-NM@users.noreply.github.com>
2022-12-02 23:02:51 +0330

    completed!

--- compiler.py
+++ compiler.py
@@ -14,26 +14,33 @@ while True:
 
 token_file = open('tokens.txt', 'w')
 
-line = tokens[0].line
-token_file.write(str(line) + '.\t')
-for t in tokens:
-    if t.line != line:
-        token_file.write('\n' + str(line) + '.\t')
-        line = t.line
-    token_file.write('(' + t.token_type + ', ' + t.lex + ') ')
+if len(tokens) != 0:
+    line = tokens[0].line
+    token_file.write(str(line) + '.\t')
+    for t in tokens:
+        if t.line != line:
+            token_file.write('\n' + str(t.line) + '.\t')
+            line = t.line
+        token_file.write('(' + t.token_type + ', ' + t.lex + ') ')
+
+token_file.write('\n')
 
 error_file = open('lexical_errors.txt', 'w')
 
-line = sc.lexical_errors[0].line
-error_file.write(str(line) + '.\t')
-for e in sc.lexical_errors:
-    if e.line != line:
-        error_file.write('\n' + str(line) + '.\t')
-    error_file.write('(' + e.lex + ', ' + e.error_type + ') ')
+if len(sc.lexical_errors) != 0:
+    line = sc.lexical_errors[0].line
+    error_file.write(str(line) + '.\t')
+    for e in sc.lexical_errors:
+        if e.line != line:
+            error_file.write('\n' + str(e.line) + '.\t')
+            line = e.line
+        error_file.write('(' + e.lex + ', ' + e.error_type + ') ')
+else:
+    error_file.write('There is no lexical error.')
 
 symbol_table_file = open('symbol_table.txt', 'w')
 
 line = 1
 for s in sc.symbol_table:
-    symbol_table_file.write(str(line) + '\t' + s + '\n')
+    symbol_table_file.write(str(line) + '.\t' + s + '\n')
     line += 1
